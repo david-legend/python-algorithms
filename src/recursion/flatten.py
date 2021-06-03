f253da1d1568233db3f85ffdf7c626978e1ad311
David <dcobbina@bridgeinnovation.io>
2021-06-03 06:20:50 +0000

    added flatten question to recursion section

--- src/recursion/flatten.py
+++ src/recursion/flatten.py
@@ -1,14 +1,22 @@
 # Flatten --> write a recursion function called flatten 
 # which accepts an array of arrays and returns a new array with all the values flattened
 
-# CapitalizFirst --> Given an array of words write a recursive function to capitalize the first letter of every word in the array
+def flatten(arr):
+    resultArr = []
+    for custItem in arr:
+        if type(custItem) is list:
+            resultArr.extend(flatten(custItem))
+        else: 
+            resultArr.append(custItem)
+    return resultArr 
 
-# NestedEvenSum --> Write a recursive function to return the sum of all even numbers in an object which may contain nested objects
 
-# CapitalizeWords --> Given an array of words write a recursive function to return a new array containing each word capitalized
 
-# StringifyNumbers --> Write a function that takes in an object and finds all the values which are numbers and converts 
-# them to strings. 
+dtSet0 = [1,2,3,4,5]
+dtSet1 = [1,2,3,[4,5]]
+dtSet2 = [1,[2,[3, 4], [[5]]]]
+dtSet3 = [[[1,[[[2]]], [[[[[[[3]]]]]]]]]]
 
-# CollectNumber --> Write a function that accepts objects and returns an array of all the 
-# values in the object that have a typeof string
+print(flatten(dtSet1))
+print(flatten(dtSet2))
+print(flatten(dtSet3))
