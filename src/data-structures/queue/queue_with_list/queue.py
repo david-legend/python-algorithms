f13b8b4c6fd3d48e42aded87bef35da3788aa857
David <dcobbina@bridgeinnovation.io>
2021-07-02 07:18:57 +0000

    added queue implementation with list

---
+++ src/data-structures/queue/queue_with_list/queue.py
@@ -0,0 +1,58 @@
+# isEmpty(), enqueue(), dequeue(), peek(), delete()
+class Queue:
+    def __init__(self):
+        super().__init__()
+        self.items = []
+        
+    def __str__(self):
+        super().__str__()
+        values = [str(x) for x in self.items]
+        return ' '.join(values)
+
+    def enqueue(self, value):
+        self.items.append(value)
+
+    def dequeue(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.items.pop(0)
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        else:
+            return self.items[0]
+
+    def isEmpty(self):
+        if self.items == []:
+            return True
+        else:
+            return False
+
+    def delete(self):
+        self.items = []
+
+
+queue = Queue()
+
+print("is empty ? ", queue.isEmpty())
+
+queue.enqueue(8)
+queue.enqueue(18)
+queue.enqueue(80)
+queue.enqueue(19)
+
+print("Current Queue: ", queue)
+print("is empty ? ", queue.isEmpty())
+
+print("Peek: ", queue.peek())
+print("Dequeue: ", queue.dequeue())
+
+print("Current Queue: ", queue)
+print("Peek: ", queue.peek())
+
+print("Dequeue: ", queue.dequeue())
+print("Peek: ", queue.peek())
+
+print("Delete: ", queue.delete())
