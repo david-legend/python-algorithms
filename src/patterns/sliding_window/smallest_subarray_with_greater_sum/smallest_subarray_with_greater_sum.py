c6426011f9b519d99d2d8843e0205ceb897566b6
David <dcobbina@bridgeinnovation.io>
2022-01-10 11:31:57 +0200

    added smallest_subarray_with_greater_sum under the sliding window pattern

---
+++ src/patterns/sliding_window/smallest_subarray_with_greater_sum/smallest_subarray_with_greater_sum.py
@@ -0,0 +1,70 @@
+import math
+
+# SOLUTION 1 (BRUTE FORCE)
+#Time complexity O(N^2)
+#Space complexity O(1)
+def smallest_subarray_with_greater_sum(arr, s):
+    k =  1
+    def _find_smallest_subarray(arr, s, k):
+        if k > len(arr):
+            return 0
+        
+        sum, start = 0.0, 0
+        for i in range(len(arr)):
+            sum  += arr[i]
+            
+            if i >= k - 1:
+                if sum >= s:
+                    return k
+                sum -= arr[start]
+                start += 1
+        
+        return _find_smallest_subarray(arr, s, k+1)
+        
+    return _find_smallest_subarray(arr, s, k)
+
+
+# SOLUTION 2
+#The time complexity of the above algorithm will be O(N). 
+#The outer for loop runs for all elements, and 
+#the inner while loop processes each element only once; therefore, 
+#the time complexity of the algorithm will be O(N+N), 
+#which is asymptotically equivalent to O(N).
+
+#Time complexity O(N)
+#Space complexity O(1)
+def smallest_subarray_with_given_sum(arr, s):
+    min_length  = math.inf
+    sum, start = 0.0, 0
+    
+    for i in range(len(arr)):
+        sum += arr[i]
+        
+        while sum >= s:
+            min_length = min(min_length, i - start + 1)
+            sum -= arr[start]
+            start += 1
+    
+    if min_length == math.inf:
+        return 0
+    
+    return min_length
+
+
+result1 = smallest_subarray_with_given_sum([2, 1, 5, 2, 3, 2], 7) #answer --> 2
+print("Smallest subarray with greater sum length: " + str(result1) + "\n\n")
+
+
+result2 = smallest_subarray_with_given_sum([2, 1, 5, 2, 8], 7) #answer --> 1
+print("Smallest subarray with greater sum length: " + str(result2) + "\n\n")
+
+
+result3 = smallest_subarray_with_greater_sum([2, 1, 5, 2, 3, 2], 7) #answer --> 2
+print("Smallest subarray with greater sum length: " + str(result3) + "\n\n")
+
+
+result4 = smallest_subarray_with_greater_sum([2, 1, 5, 2, 8], 7) #answer --> 1
+print("Smallest subarray with greater sum length: " + str(result4) + "\n\n")
+        
+    
+                
