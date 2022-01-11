c7ec430113665bf6ef498acd4693af43cea078c2
David <dcobbina@bridgeinnovation.io>
2022-01-11 12:06:25 +0200

    added triplet sum to zero

---
+++ src/patterns/two_pointers/triplet_sum_to_zero/README.md
@@ -0,0 +1,14 @@
+# Triplet Sum to Zero (medium) ✩
+
+Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
+
+### Example 1
+Input: [-3, 0, 1, 2, -1, 1, -2]
+Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
+Explanation: There are four unique triplets whose sum is equal to zero.
+
+### Example 2
+Input: [-5, 2, -1, -2, 3]
+Output: [[-5, 2, 3], [-2, -1, 3]]
+Explanation: There are two unique triplets whose sum is equal to zero.
+
---
+++ src/patterns/two_pointers/triplet_sum_to_zero/triplet_sum_to_zero.py
@@ -0,0 +1,44 @@
+# Time complexity for the algorithms below will be O(N^2)
+# Sorting the array will take O(N * logN). 
+# The searchPair() function will take O(N). 
+# As we are calling searchPair() for every number in the input array, 
+# this means that overall searchTriplets() will take O(N * logN + N^2), 
+# which is asymptotically equivalent to O(N^2).
+
+# Space complexity will be O(N)
+# Ignoring the space required for the output array, 
+# the space complexity of the above algorithm will be O(N) 
+# which is required for sorting.
+
+def search_triplets(arr):
+    arr.sort()
+    triplets = []
+    
+    for i in range(len(arr)):
+        if i > 0 and arr[i] == arr[i-1]:
+            continue
+        search_pair(arr, -arr[i], i+1, triplets)
+    
+    return triplets
+
+def search_pair(arr, target_sum, left, triplets):
+    right = len(arr) - 1
+    while left < right:
+        sum = arr[left] + arr[right]
+        if sum == target_sum:
+            triplets.append([-target_sum, arr[left], arr[right]])
+            left += 1
+            right -= 1
+            
+            while left < right and arr[left] == arr[left-1]:
+                left += 1
+            while left < right and arr[right] == arr[right+1]:
+                right -= 1
+        elif sum < target_sum:
+            left += 1
+        else:
+            right -= 1
+            
+            
+print(search_triplets([-3, 0, 1, 2, -1, 1, -2]))
+print(search_triplets([-5, 2, -1, -2, 3]))
