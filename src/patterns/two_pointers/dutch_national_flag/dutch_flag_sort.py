a8083f04839cfbfbca9f81de002cf633bb4325ac
David <dcobbina@bridgeinnovation.io>
2022-01-16 09:34:08 +0200

    added solution for the dutch national flag question

--- src/patterns/two_pointers/dutch_national_flag/dutch_flag_sort.py
+++ src/patterns/two_pointers/dutch_national_flag/dutch_flag_sort.py
@@ -1,3 +1,20 @@
 def dutch_flag_sort(arr):
-    # TODO: Write your code here
-    return 
+    low, high = 0, len(arr) - 1
+    i = 0
+    
+    while i <= high:
+        if arr[i] == 0:
+            arr[i], arr[low] = arr[low], arr[i]
+            low += 1
+            i += 1
+        elif arr[i] == 1:
+            i += 1
+        else:
+            arr[i], arr[high] = arr[high], arr[i]
+            high -= 1
+    
+    return arr
+
+
+print(dutch_flag_sort([1, 0, 2, 1, 0]))  #Output: [0, 0, 1, 1, 2]
+print(dutch_flag_sort([2, 2, 0, 1, 2, 0])) #Output: [0, 0, 1, 2, 2, 2,]
