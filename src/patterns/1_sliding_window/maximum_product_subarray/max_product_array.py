f6cc76cf00d2c4ac273e45faa9a4fd4296fb977d
David <dcobbina@bridgeinnovation.io>
2022-01-20 12:59:16 +0200

    added max product subarray changes

--- src/patterns/1_sliding_window/maximum_product_subarray/max_product_array.py
+++ src/patterns/1_sliding_window/maximum_product_subarray/max_product_array.py
@@ -5,15 +5,25 @@ def maximum_product_subarray(arr):
     
     for window_end in range(len(arr)):
         product *= arr[window_end]
-        if product < max_product:
-            window_start = max(window_start, window_end)
-            product = arr[window_start]
+        
+        while product < max_product and window_start < window_end:
+            if arr[window_start] == 0:
+                window_start += 1
+            else:
+                product = product / arr[window_start]
+                window_start += 1
         
         max_product = max(max_product, product)
         
+        if product == 0:
+           window_start = max(window_start, window_end)
+           product = arr[window_start]
+           
     return max_product
 
 
 print(maximum_product_subarray([1,2,-1,4,6]))
 print(maximum_product_subarray([-1,-2,-1,-4,6]))
 print(maximum_product_subarray([-1,6,2,0,7,9]))
+print(maximum_product_subarray([-4, -3, -5]))
+print(maximum_product_subarray([-2,0,3,5])) 
