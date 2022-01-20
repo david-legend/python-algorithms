eb9021591613bacb57a2f98840b347d3797f6419
David <dcobbina@bridgeinnovation.io>
2022-01-20 18:09:45 +0200

    added find duplicates

---
+++ src/patterns/5_cyclic_sort/find_all_duplicate_numbers/find_duplicates.py
@@ -0,0 +1,25 @@
+# Time complexity O(N)
+# The time complexity of the above algorithm is O(N).
+
+# Space complexity O(1)
+# Ignoring the space required for storing the duplicates, 
+# the algorithm runs in constant space O(1).
+def find_all_duplicates(nums):
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
+
+    duplicateNumbers = []
+    for i in range(len(nums)):
+        if nums[i] != i + 1:
+            duplicateNumbers.append(nums[i])
+
+    return duplicateNumbers
+
+
+print(find_all_duplicates([3, 4, 4, 5, 5]))
+print(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]))
