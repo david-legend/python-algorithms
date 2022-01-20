ceda6c927e63758c38c45449f2f380ee33dad972
David <dcobbina@bridgeinnovation.io>
2022-01-20 14:17:32 +0200

    added time and space complexity explanations for cyclic sort

--- src/patterns/5_cyclic_sort/cyclic_sort/cyclic_sort.py
+++ src/patterns/5_cyclic_sort/cyclic_sort/cyclic_sort.py
@@ -1,3 +1,15 @@
+# Time complexity O(n)
+# The time complexity of the algorithm is O(n). 
+# Although we are not incrementing the index i when swapping the numbers, 
+# this will result in more than n iterations of the loop, 
+# but in the worst-case scenario, the while loop will swap a total of n-1 numbers, 
+# and once a number is at its correct index, 
+# we will move on to the next number by incrementing i. 
+# So overall, our algorithm will take O(n) + O(n-1) 
+# which is asymptotically equivalent to O(n).
+
+# Space complexity O(1)
+# The algorithm runs in constant space O(1).
 def cyclic_sort(nums):
     i = 0
     while i < len(nums):
