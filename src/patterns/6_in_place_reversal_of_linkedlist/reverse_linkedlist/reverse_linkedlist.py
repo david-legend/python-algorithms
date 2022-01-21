5c48c1e25b04e798142c67aa2b709fad5f3f927f
David <dcobbina@bridgeinnovation.io>
2022-01-21 14:26:59 +0200

    added reverse linkedlist

---
+++ src/patterns/6_in_place_reversal_of_linkedlist/reverse_linkedlist/reverse_linkedlist.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+import re
+
+
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+  def print_list(self):
+    temp = self
+    while temp is not None:
+      print(temp.value, end=" ")
+      temp = temp.next
+    print()
+  
+def reverse(head):
+    if head:
+        curr_node = head
+        prev = None
+        
+        while curr_node:
+            next = curr_node.next  
+            curr_node.next = prev
+            prev = curr_node
+            curr_node = next
+        
+        head = prev
+        return head
+    
+head = Node(2)
+head.next = Node(4)
+head.next.next = Node(6)
+head.next.next.next = Node(8)
+head.next.next.next.next = Node(10)
+
+print("Nodes of original LinkedList are: ", end='')
+head.print_list()
+result = reverse(head)
+print("Nodes of reversed LinkedList are: ", end='')
+result.print_list()
