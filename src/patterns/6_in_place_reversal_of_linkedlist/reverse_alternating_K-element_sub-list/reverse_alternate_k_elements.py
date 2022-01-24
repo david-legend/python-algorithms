96b549ffa49bd12c7a5136f68a28fbeef6e50253
David <dcobbina@bridgeinnovation.io>
2022-01-24 10:17:12 +0200

    added reverse_alternate_k_elements solution

--- src/patterns/6_in_place_reversal_of_linkedlist/reverse_alternating_K-element_sub-list/reverse_alternate_k_elements.py
+++ src/patterns/6_in_place_reversal_of_linkedlist/reverse_alternating_K-element_sub-list/reverse_alternate_k_elements.py
@@ -12,10 +12,45 @@ class Node:
             temp = temp.next
         print()
 
+# Time complexity
+# The time complexity of our algorithm will be O(N) 
+# where ‘N’ is the total number of nodes in the LinkedList.
 
+# Space complexity
+# We only used constant space, therefore, 
+# the space complexity of our algorithm is O(1).
 def reverse_alternate_k_elements(head, k):
-  # TODO: Write your code here
-  return head
+    if k <= 1 or head is None:
+        return head
+
+    previous, current = None, head
+
+    while current is not None:
+        last_node_of_prev_part = previous
+        last_node_of_sublist = current
+        
+        i, next = 0, None
+        while current is not None and i < k:
+            next = current.next
+            current.next = previous
+            previous = current
+            current = next
+            i += 1
+        
+        if last_node_of_prev_part is not None:
+            last_node_of_prev_part.next = previous
+        else:
+            head = previous
+        
+        last_node_of_sublist.next = current
+        
+        i = 0
+        while current is not None and i < k:
+            previous = current
+            current = current.next
+            i += 1
+        
+    return head
 
 
 head = Node(1)
