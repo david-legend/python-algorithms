8067885b0e0efc7cf5dfc9c9fcddfc50dd968ebb
David <dcobbina@bridgeinnovation.io>
2022-01-26 09:31:25 +0200

    added btree path sum solution"

--- src/patterns/8_tree_depth_first_search/btree_path_sum/btree_path_sum.py
+++ src/patterns/8_tree_depth_first_search/btree_path_sum/btree_path_sum.py
@@ -4,10 +4,27 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# Time complexity
+# The time complexity of the above algorithm is O(N), 
+# where ‘N’ is the total number of nodes in the tree. 
+# This is due to the fact that we traverse each node once.
+
+# Space complexity
+# The space complexity of the above algorithm will be O(N) in the worst case. 
+# This space will be used to store the recursion stack. 
+# The worst case will happen when the given tree is a linked list (i.e., every node has only one child).
 
 def has_path(root, sum):
-    # TODO: Write your code here
-    return False
+    if root is None:
+        return False
+    
+    # if the current node is a leaf and its value is equal to the sum, we've found a path
+    if root.val == sum and root.left is None and root.right is None:
+        return True
+    
+    # recursively call to traverse the left and right sub-tree
+    # return true if any of the two recursive call return true
+    return has_path(root.left, sum - root.val) or has_path(root.right, sum - root.val)
 
 
 
