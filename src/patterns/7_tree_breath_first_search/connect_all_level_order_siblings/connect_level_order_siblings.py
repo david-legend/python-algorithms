a905a768275b28ee5d234951722bc68667b475c1
David <dcobbina@bridgeinnovation.io>
2022-01-26 08:35:30 +0200

    added connect all level order siblings solution

--- src/patterns/7_tree_breath_first_search/connect_all_level_order_siblings/connect_level_order_siblings.py
+++ src/patterns/7_tree_breath_first_search/connect_all_level_order_siblings/connect_level_order_siblings.py
@@ -1,8 +1,20 @@
 from __future__ import print_function
 from collections import deque
-from logging import RootLogger
 
 
+
+# Time complexity
+# The time complexity of the above algorithm is O(N), 
+# where ‘N’ is the total number of nodes in the tree. 
+# This is due to the fact that we traverse each node once.
+
+# Space complexity
+# The space complexity of the above algorithm will be O(N) 
+# as we need to return a list containing the level order traversal. 
+# We will also need O(N) space for the queue. 
+# Since we can have a maximum of N/2 nodes at any level 
+# (this could happen only at the lowest level), 
+# therefore we will need O(N) space to store them in the queue.
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -18,9 +30,27 @@ class TreeNode:
 
 
 def connect_all_siblings(root):
-    return root
+    if root is None:
+        return
+    
+    queue = deque()
+    queue.append(root)
+    prev_node, curr_node = None, None
+    
+    while queue:
+        curr_node = queue.popleft()
+        
+        if  prev_node:
+            prev_node.next = curr_node
+        prev_node = curr_node 
+        
+        if curr_node.left:
+            queue.append(curr_node.left)
 
-
+        if curr_node.right:
+            queue.append(curr_node.right)
+            
+            
 root = TreeNode(12)
 root.left = TreeNode(7)
 root.right = TreeNode(1)
--- src/patterns/7_tree_breath_first_search/connect_level_order_siblings/connect_level_order_siblings.py
+++ src/patterns/7_tree_breath_first_search/connect_level_order_siblings/connect_level_order_siblings.py
@@ -2,6 +2,18 @@ from __future__ import print_function
 from collections import deque
 
 
+# Time complexity
+# The time complexity of the above algorithm is O(N), 
+# where ‘N’ is the total number of nodes in the tree. 
+# This is due to the fact that we traverse each node once.
+
+# Space complexity
+# The space complexity of the above algorithm will be O(N) 
+# as we need to return a list containing the level order traversal. 
+# We will also need O(N) space for the queue. 
+# Since we can have a maximum of N/2 nodes at any level 
+# (this could happen only at the lowest level), 
+# therefore we will need O(N) space to store them in the queue.
 class TreeNode:
     def __init__(self, val):
         self.val = val
