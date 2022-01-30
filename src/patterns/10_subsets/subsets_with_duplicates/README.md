0750395015a057de887c30623e6bf23b7bb31e52
David <dcobbina@bridgeinnovation.io>
2022-01-30 10:13:30 +0200

    added subsets with duplicates to array

---
+++ src/patterns/10_subsets/subsets_with_duplicates/README.md
@@ -0,0 +1,11 @@
+# Subsets With Duplicates (easy) ✩
+
+Given a set with distinct elements, find all of its distinct subsets.
+
+### Example 1
+Input: [1, 3, 3]
+Output: [], [1], [3], [1,3], [3,3], [1,3,3]
+
+### Example 2
+Input: [1, 5, 3, 3]
+Output: [], [1], [5], [3], [1,5], [1,3], [5,3], [1,5,3], [3,3], [1,3,3], [3,3,5], [1,5,3,3] 
--- src/patterns/10_subsets/subsets_with_duplicates/subsets_with_duplicates.py
+++ src/patterns/10_subsets/subsets_with_duplicates/subsets_with_duplicates.py
@@ -12,19 +12,24 @@
 # O(N) space, therefore, the space complexity of our algorithm will be O(N*2^N).
 
 def find_subsets(nums):
+    nums.sort()
     subsets = []
     subsets.append([])
+    start_index, end_index = 0, 0
     
-    for curr_number in nums:
-        n = len(subsets)
+    for i in range(len(nums)):
+        start_index = 0
+        if i > 0 and nums[i] == nums[i-1]:
+            start_index = end_index + 1
         
-        for i in range(n):
-            set_1 = list(subsets[i])
-            set_1.append(curr_number)
-            subsets.append(set_1)
-            
+        end_index = len(subsets) - 1
+        for j in range(start_index, end_index+1):
+            curr_set = list(subsets[j])
+            curr_set.append(nums[i])
+            subsets.append(curr_set)
+    
     return subsets
-            
-
-print("Here is the list of subsets: " + str(find_subsets([1, 3])))
-print("Here is the list of subsets: " + str(find_subsets([1, 5, 3])))
+    
+    
+print("Here is the list of subsets: " + str(find_subsets([1, 3, 3])))
+print("Here is the list of subsets: " + str(find_subsets([1, 5, 3, 3])))
