e98380b3514ee3e624ca4e5d09b9d4125a40e37f
David <dcobbina@bridgeinnovation.io>
2022-02-03 10:07:00 +0200

    addded k closest point to origin solution

---
+++ src/patterns/12_top_k_elements/k_closest_point_to_origin/README.md
@@ -0,0 +1,15 @@
+# 'K' Closest Points to the Origin (easy) ✩
+
+Given an array of points in a 2D plane, find ‘K’ closest points to the origin.
+
+### Example 1
+Input: points = [[1,2],[1,3]], K = 1
+Output: [[1,2]]
+Explanation: The Euclidean distance between (1, 2) and the origin is sqrt(5).
+The Euclidean distance between (1, 3) and the origin is sqrt(10).
+Since sqrt(5) < sqrt(10), therefore (1, 2) is closer to the origin.
+
+### Example 2
+Input: point = [[1, 3], [3, 4], [2, -1]], K = 2
+Output: [[1, 3], [2, -1]]
+
---
+++ src/patterns/12_top_k_elements/k_closest_point_to_origin/k_closest_point_to_origin.py
@@ -0,0 +1,46 @@
+from heapq import *
+
+class Point:
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.distance = self.distance_from_origin()
+
+    # used for max-heap
+    def __lt__(self, other):
+        return self.distance > other.distance
+
+    def distance_from_origin(self):
+        # ignoring sqrt to calculate the distance
+        return (self.x * self.x) + (self.y * self.y)
+
+    def print_point(self):
+        print("[" + str(self.x) + ", " + str(self.y) + "] ", end='')
+  
+        
+def find_closest_points(points, k):
+    max_heap = []
+    
+    for i in range(k):
+        heappush(max_heap, points[i])
+    
+    for i in range(k, len(points)):
+        if points[i].distance < max_heap[0].distance:
+            heappop(max_heap)
+            heappush(max_heap, points[i])
+    
+    return list(max_heap)
+
+
+test_1 = find_closest_points([Point(1, 3), Point(3, 4), Point(2, -1)], 2)
+print("Here are the k points closest the origin: ", end='')
+for point in test_1:
+    point.print_point()
+ 
+print("\n\n")
+    
+test_2 = find_closest_points([Point(1, 2), Point(1, 3)], 1)
+print("Here are the k points closest the origin: ", end='')
+for point in test_2:
+    point.print_point()
