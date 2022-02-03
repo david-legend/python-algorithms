32f2e05b2b573d66fe988bc0856004772e750c8d
David <dcobbina@bridgeinnovation.io>
2022-02-03 18:48:37 +0200

    added solution for k closest numbers

---
+++ src/patterns/12_top_k_elements/k_closest_numbers/README.md
@@ -0,0 +1,18 @@
+# 'K' Closest Numbers (medium) ✩
+
+Given a sorted number array and two integers ‘K’ and ‘X’, 
+find ‘K’ closest numbers to ‘X’ in the array. Return the numbers in the sorted order. 
+‘X’ is not necessarily present in the array.
+
+
+### Example 1
+Input: [5, 6, 7, 8, 9], K = 3, X = 7
+Output: [6, 7, 8]
+
+### Example 2
+Input: [2, 4, 5, 6, 9], K = 3, X = 6
+Output: [4, 5, 6]
+
+### Example 3
+Input: [2, 4, 5, 6, 9], K = 3, X = 10
+Output: [5, 6, 9]
---
+++ src/patterns/12_top_k_elements/k_closest_numbers/k_closest_numbers.py
@@ -0,0 +1,47 @@
+from heapq import *
+
+
+def find_closest_elements(arr, K, X):
+    index = binary_search(arr, X)
+    left, right = index - K, index + K
+    
+    left = max(left, 0)
+    right = min(right, len(arr) - 1)
+    
+    min_heap = []
+    for i in range(left, right+1):
+        heappush(min_heap, (abs(arr[i] - X), arr[i]))
+     
+    result = []   
+    for i in range(K):
+        result.append(heappop(min_heap)[1])
+        
+    result.sort()
+    return result
+
+
+def binary_search(arr, key):
+    start, end = 0, len(arr) - 1
+    
+    while start <= end:
+        mid = start + (end - start) // 2
+        if key == arr[mid]:
+            return mid
+        
+        if key > arr[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+    
+    if start > 0:
+        return start - 1
+    
+    return start
+
+
+print("'K' closest numbers to 'X' are: " +
+        str(find_closest_elements([5, 6, 7, 8, 9], 3, 7)))
+print("'K' closest numbers to 'X' are: " +
+    str(find_closest_elements([2, 4, 5, 6, 9], 3, 6)))
+print("'K' closest numbers to 'X' are: " +
+    str(find_closest_elements([2, 4, 5, 6, 9], 3, 10)))
