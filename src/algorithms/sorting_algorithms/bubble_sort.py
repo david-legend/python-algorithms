c62d306050a201ec0b90195e8023bd3de6e8ccfe
David <dcobbina@bridgeinnovation.io>
2022-02-15 09:48:12 +0200

    added bubble sort and insertion sort

---
+++ src/algorithms/sorting_algorithms/bubble_sort.py
@@ -0,0 +1,24 @@
+from more_itertools import is_sorted
+
+
+def bubble_sort(array):
+    is_sorted = False
+    
+    while not is_sorted:
+        is_sorted = True
+        for i in range(len(array) - 1):
+            if array[i] > array[i + 1]:
+                swap(i, i+1, array)
+                is_sorted = False
+    
+    return array  
+        
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
+    
+
+print(bubble_sort([8, 5, 2, 9, 5, 6, 3]))
+print(bubble_sort([1]))
+print(bubble_sort([3, 1, 2]))
+print(bubble_sort([1, 2, 3]))
---
+++ src/algorithms/sorting_algorithms/insertion_sort.py
@@ -0,0 +1,19 @@
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            swap(j, j-1, array)
+            j -= 1
+
+    return array
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
+    
+    
+
+print(insertion_sort([8, 5, 2, 9, 5, 6, 3]))
+print(insertion_sort([1]))
+print(insertion_sort([3, 1, 2]))
+print(insertion_sort([1, 2, 3]))
