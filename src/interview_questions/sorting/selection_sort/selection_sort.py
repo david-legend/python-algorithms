092a6cfdc29fc094a66684bf0c5bc6bdb4c4b118
David <dcobbina@bridgeinnovation.io>
2022-02-17 11:09:39 +0200

    rewrote sorting algorithm

--- src/interview_questions/sorting/selection_sort/selection_sort.py
+++ src/interview_questions/sorting/selection_sort/selection_sort.py
@@ -1,15 +1,17 @@
+
 def selection_sort(array):
+    n = len(array)
     current_idx = 0
     
-    while current_idx < len(array) - 1:
-        smallest_idx = current_idx
-        for i in range(current_idx+1, len(array)):
-            if array[i] < array[smallest_idx]:
-                smallest_idx = i
+    for i in range(current_idx, n-1):
+        smallest_idx = i
+        for j in range(current_idx+1, n):
+            if array[smallest_idx] > array[j]:
+                smallest_idx = j
         
-        swap(current_idx, smallest_idx, array)
+        swap(smallest_idx, current_idx, array)
         current_idx += 1
-    
+                
     return array
 
 def swap(i, j, array):
