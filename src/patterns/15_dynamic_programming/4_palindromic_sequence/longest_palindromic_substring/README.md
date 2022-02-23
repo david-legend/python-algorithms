cf1d7847498e7b43c7699f4653a8d0045b22abec
David <dcobbina@bridgeinnovation.io>
2022-02-23 18:55:23 +0200

    added lp substring top down approach

---
+++ src/patterns/15_dynamic_programming/4_palindromic_sequence/longest_palindromic_substring/README.md
@@ -0,0 +1,22 @@
+# Longest Palindromic Substring
+
+Given a string, find the length of its Longest Palindromic Substring (LPS). 
+In a palindromic string, elements read the same backward and forward.
+
+
+
+### Example 1:
+Input: "abdbca"
+Output: 3
+Explanation: LPS is "bdb".
+
+### Example 2:
+Input: = "cddpd"
+Output: 3
+Explanation: LPS is "dpd".
+
+### Example 3:
+Input: = "pqr"
+Output: 1
+Explanation: LPS could be "p", "q" or "r".
+
--- src/patterns/15_dynamic_programming/4_palindromic_sequence/longest_palindromic_substring/lp_substring_top_down_solution.py
+++ src/patterns/15_dynamic_programming/4_palindromic_sequence/longest_palindromic_substring/lp_substring_top_down_solution.py
@@ -2,6 +2,7 @@ def find_LPS_length(str):
     n = len(str)
     return find_LPS_length_recursive(str, 0, n-1)
 
+
 def find_LPS_length_recursive(str, start_idx, end_idx):
     if start_idx > end_idx:
         return 0
@@ -10,38 +11,46 @@ def find_LPS_length_recursive(str, start_idx, end_idx):
         return 1
     
     if str[start_idx] == str[end_idx]:
-        return 2 + find_LPS_length_recursive(str, start_idx + 1, end_idx - 1)
+        remaining_length = end_idx - start_idx - 1
+        if remaining_length == find_LPS_length_recursive(str,  start_idx + 1, end_idx - 1):
+            return remaining_length + 2
+         
     
-    l1 = find_LPS_length_recursive(str, start_idx + 1, end_idx)
-    l2 = find_LPS_length_recursive(str, start_idx, end_idx - 1)
+    l1 = find_LPS_length_recursive(str,  start_idx + 1, end_idx)
+    l2 = find_LPS_length_recursive(str,  start_idx, end_idx - 1)
     
     return max(l1, l2)
-
+    
 
 def find_LPS_length_dp(str):
     n = len(str)
-
     dp = [[-1 for _ in range(n)] for _ in range(n)]
+    
     return find_LPS_length_recursive_dp(dp, str, 0, n-1)
 
 def find_LPS_length_recursive_dp(dp, str, start_idx, end_idx):
     if start_idx > end_idx:
         return 0
-    
+
     if start_idx == end_idx:
         return 1
-    
+
     if dp[start_idx][end_idx] != -1:
         return dp[start_idx][end_idx]
-    
+
     if str[start_idx] == str[end_idx]:
-        dp[start_idx][end_idx] = 2 + find_LPS_length_recursive_dp(dp, str, start_idx + 1, end_idx - 1)
-        return dp[start_idx][end_idx]
-    
+        remaining_length = end_idx - start_idx - 1
+        
+        if remaining_length == find_LPS_length_recursive_dp(dp, str, start_idx + 1, end_idx - 1):
+            dp[start_idx][end_idx] = remaining_length + 2
+            return dp[start_idx][end_idx]
+
     l1 = find_LPS_length_recursive_dp(dp, str, start_idx + 1, end_idx)
     l2 = find_LPS_length_recursive_dp(dp, str, start_idx, end_idx - 1)
     dp[start_idx][end_idx] = max(l1, l2)
+
     return dp[start_idx][end_idx]
+
 
 def main():
     print("Recursive Solution Without Memoization")
