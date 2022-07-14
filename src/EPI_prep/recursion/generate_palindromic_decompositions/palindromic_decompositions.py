f781467f045a122a58cf7464a7cfc333d3c98ac6
David Cobbina <davidcobbina47@gmail.com>
2022-07-14 07:47:39 +0000

    added palindromic decompositions solution

---
+++ src/EPI_prep/recursion/generate_palindromic_decompositions/palindromic_decompositions.py
@@ -0,0 +1,27 @@
+def palindromic_decompostions(input):
+    def directed_palindromic_decompostions(i):
+        if i >= len(input):
+            result.append(list(partition))
+
+        for j in range(i, len(input)):
+            if is_palindrome(input, i, j):
+                partition.append(input[i:j+1])
+                directed_palindromic_decompostions(j+1)
+                partition.pop()
+            
+    result, partition = [], []
+    directed_palindromic_decompostions(0)
+
+    return result
+
+def is_palindrome(input, left, right):
+    while left < right:
+        if input[left] != input[right]:
+            return False
+        left, right = left + 1, right - 1
+    
+    return True
+
+print(palindromic_decompostions("1414")) # [['1', '4', '1', '4'], ['1', '414'], ['141', '4']]
+print(palindromic_decompostions("1234")) #[['1', '2', '3', '4']]
+print(palindromic_decompostions("0204451881")) # [['0', '2', '0', '4', '4', '5', '1', '8', '8', '1'], ['0', '2', '0', '4', '4', '5', '1', '88', '1'], ['0', '2', '0', '4', '4', '5', '1881'], ['0', '2', '0', '44', '5', '1', '8', '8', '1'], ['0', '2', '0', '44', '5', '1', '88', '1'], ['0', '2', '0', '44', '5', '1881'], ['020', '4', '4', '5', '1', '8', '8', '1'], ['020', '4', '4', '5', '1', '88', '1'], ['020', '4', '4', '5', '1881'], ['020', '44', '5', '1', '8', '8', '1'], ['020', '44', '5', '1', '88', '1'], ['020', '44', '5', '1881']]
