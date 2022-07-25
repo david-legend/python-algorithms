3af526088375cf82fd76763167454a35d8b2e1ef
David Cobbina <davidcobbina47@gmail.com>
2022-07-25 06:24:01 +0000

    added time complexities

---
+++ src/EPI_prep/binary_trees/8_preorder_traversal_without_recursion/README.md
@@ -0,0 +1,4 @@
+## Implement an Preorder traversal without recursion
+
+* Write a program which takes as input a binary tree and performs an preorder traversal of the tree. 
+Do not use recursion. Nodes do not contain parent references.
--- src/EPI_prep/binary_trees/8_preorder_traversal_without_recursion/preorder_traversal.py
+++ src/EPI_prep/binary_trees/8_preorder_traversal_without_recursion/preorder_traversal.py
@@ -7,20 +7,20 @@ class Node:
 # Space: O(h) - best case
 # Space: O(n) - worst case - skewed tree
 
-def inorder_traversal(tree):
+
+def preorder_traversal(tree):
     path, result = [], []
 
     while tree or path:
         if tree:
+            result.append(tree.val)
             path.append(tree)
             tree = tree.left
         else:
             tree = path.pop()
-            result.append(tree.val)
             tree = tree.right
     
     return result
-
 
 node_1 = Node(1)
 node_2 = Node(2)
@@ -34,4 +34,4 @@ node_1.left, node_1.right = node_2, node_3
 node_2.left, node_2.right = node_4, node_5
 node_3.left, node_3.right = node_6, node_7
 
-print(inorder_traversal(node_1)) # [4, 2, 5, 1, 6, 3, 7]
+print(preorder_traversal(node_1)) # [1, 2, 4, 5, 3, 6, 7]
