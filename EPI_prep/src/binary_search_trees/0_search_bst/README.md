dcb98e43699bea9df1df3b6d8e27b25325d41ecd
David Cobbina <davidcobbina47@gmail.com>
2022-08-03 09:21:44 +0000

    added search bst solution

---
+++ EPI_prep/src/binary_search_trees/0_search_bst/README.md
@@ -0,0 +1,3 @@
+# Search Binary Search Tree
+
+- Write a function to check if a given value exists in a BST
---
+++ EPI_prep/src/binary_search_trees/0_search_bst/search_bst.py
@@ -0,0 +1,52 @@
+class TreeNode:
+    def __init__(self, val=None, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def search_bst(tree, key):
+    if tree :
+        if tree.val == key:
+            return tree
+        elif key <= tree.val:
+            return search_bst(tree.left, key)
+        else:
+            return search_bst(tree.right, key)
+
+
+bst = TreeNode(19)
+bst_1 = TreeNode(7)
+bst_2 = TreeNode(43)
+bst_3 = TreeNode(3)
+bst_4 = TreeNode(11)
+bst_5 = TreeNode(23)
+bst_6 = TreeNode(47)
+bst_7 = TreeNode(2)
+bst_8 = TreeNode(5)
+bst_9 = TreeNode(17)
+bst_10 = TreeNode(37)
+bst_11 = TreeNode(53)
+bst_12 = TreeNode(13)
+bst_13 = TreeNode(29)
+bst_14 = TreeNode(41)
+bst_15 = TreeNode(31)
+
+bst.left, bst.right = bst_1, bst_2
+# Left Subtree
+bst_1.left, bst_1.right = bst_3, bst_4
+bst_3.left, bst_3.right = bst_7, bst_8
+bst_9.left, bst_4.right = bst_12, bst_9
+
+# Right Subtree
+bst_2.left, bst_2.right = bst_5, bst_6
+bst_5.right = bst_10
+bst_10.left, bst_10.right = bst_13, bst_14
+bst_13.right = bst_15
+bst_6.right = bst_11
+
+
+print(search_bst(bst, 19).val)
+print(search_bst(bst, 7).val)
+print(search_bst(bst, 31).val)
+print(search_bst(bst, 13).val)
+print(search_bst(bst, 69))
