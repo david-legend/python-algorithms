84c8b5152521124117afa65893cff7d29fe110ba
David Cobbina <davidcobbina47@gmail.com>
2022-08-20 09:58:36 +0000

    added add two lists solution

--- EPI_prep/src/linkedlist/13_add_two_linkedlists/add_lists.py
+++ EPI_prep/src/linkedlist/13_add_two_linkedlists/add_lists.py
@@ -1,10 +1,24 @@
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val, self.next = val, next
 
-# Time O(n) | Space O(1)
-def add_list(L1, l2):
-    pass
+# Time O(n + m) | Space O(max(n, m))
+# where n and m are the lengths of the two lists.
+def add_list(L1, L2):
+    result = result_iter = ListNode(0)
+    carry = 0
+
+    while L1 or L2 or carry:
+        sum = carry + (L1.val if L1 else 0) + (L2.val if L2 else 0)
+        result_iter.next = ListNode(sum % 10)
+        carry = sum // 10
+        L1 = L1.next if L1 else None
+        L2 = L2.next if L2 else None
+        result_iter = result_iter.next
+    
+    return result.next
 
 head1 = ListNode(3)
 node_1 = ListNode(1)
@@ -26,9 +40,10 @@ def print_node(node):
 
 
 
-print_node(add_list(head1, head2)) # 1320
+# add 413 + 907 = 1320  --> 0231
+print_node(add_list(head1, head2)) # 0231
 
 node_4.next = ListNode(7)
 
-
-print_node(add_list(head1, head2)) # 1320
+# add 413 + 7907 = 8320  --> 0238
+print_node(add_list(head1, head2)) # 0238
