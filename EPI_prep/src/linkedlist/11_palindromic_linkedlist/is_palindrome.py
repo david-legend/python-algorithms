e619b3c02cb0398d812d6b544ae7ee25b0b729ef
David Cobbina <davidcobbina47@gmail.com>
2022-08-20 08:41:53 +0000

    added palindromic linkedlist solution

--- EPI_prep/src/linkedlist/11_palindromic_linkedlist/is_palindrome.py
+++ EPI_prep/src/linkedlist/11_palindromic_linkedlist/is_palindrome.py
@@ -4,7 +4,30 @@ class ListNode:
 
 # Time O(n) | Space O(1)
 def isPalindrome(L):
-    pass
+    if not L or L.next is None:
+        return True
+
+    slow = fast = L
+    while fast and fast.next:
+        slow, fast = slow.next, fast.next.next
+    
+    s1, s2 = reverse_linkedlist(slow), L
+
+    while s1.next and s2.next:
+        if s1.val != s2.val:
+            return False
+        s1, s2 = s1.next, s2.next
+
+    return True
+
+def reverse_linkedlist(node):
+    curr_node, prev_node = node, None
+    while curr_node:
+        next_node = curr_node.next
+        curr_node.next = prev_node
+        prev_node, curr_node = curr_node, next_node
+    
+    return prev_node
 
 head = ListNode(1)
 node_1 = ListNode(3)
@@ -18,17 +41,8 @@ node_2.next, node_3.next = node_3, node_4
 node_4.next, node_5.next = node_5, node_6
 
 
-def print_node(node):
-    while node:
-        print(node.val, end=" ")
-        node = node.next
-    
-    print("\n")
-
-
-
-print_node(isPalindrome(head)) # True
+print(isPalindrome(head)) # True
 node_4.val = 19
 
-print_node(isPalindrome(head)) # False
-print_node(isPalindrome(ListNode(16))) # True
+print(isPalindrome(head)) # False
+print(isPalindrome(ListNode(16))) # True
