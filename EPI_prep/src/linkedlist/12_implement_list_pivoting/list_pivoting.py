7d7d3f5d78949ddd7149c98741182a04bf6379d5
David Cobbina <davidcobbina47@gmail.com>
2022-08-20 09:35:07 +0000

    added list pivoting solution'

--- EPI_prep/src/linkedlist/12_implement_list_pivoting/list_pivoting.py
+++ EPI_prep/src/linkedlist/12_implement_list_pivoting/list_pivoting.py
@@ -5,7 +5,34 @@ class ListNode:
 
 # Time O(n) | Space O(1)
 def list_pivot(L, x):
-    pass
+    if not L:
+        return L
+    
+    left_head = left_node_it = ListNode(0)
+    pivot_head = pivot_node_it = ListNode(0)
+    right_head = right_node_it = ListNode(0)
+
+    while L:
+        if L.val < x:
+            left_node_it.next = L
+            left_node_it = left_node_it.next
+        elif L.val == x:
+            pivot_node_it.next = L
+            pivot_node_it = pivot_node_it.next
+        else:
+            right_node_it.next = L
+            right_node_it = right_node_it.next
+        L = L.next
+
+    right_node_it.next = None
+    pivot_node_it.next = right_head.next
+    left_node_it.next = pivot_head.next
+
+    return left_head.next
+
+
+
+
 
 head = ListNode(3)
 node_1 = ListNode(2)
@@ -32,4 +59,5 @@ print_node(head)
 
 # After Merge
 print_node(list_pivot(head, 7)) # [3,2,2,5,7,11,11]
+print_node(list_pivot(ListNode(1), -100)) # [3,2,2,5,7,11,11]
 
