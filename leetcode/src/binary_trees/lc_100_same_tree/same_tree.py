b521f27950155123e566e1afe17e215d78549b2f
David Cobbina <davidcobbina47@gmail.com>
2022-08-30 06:57:48 +0000

    added is same tree solution

--- leetcode/src/binary_trees/lc_100_same_tree/same_tree.py
+++ leetcode/src/binary_trees/lc_100_same_tree/same_tree.py
@@ -4,5 +4,32 @@ class Node:
         self.left = self.right = None
 
 
-def same_tree(root):
-    pass
+def same_tree(p, q):
+    def same_tree_helper(nodeP, nodeQ):
+        if nodeP is None and nodeQ is None:
+            return True
+        
+        if nodeP is None or nodeQ is None:
+            return False
+
+        if nodeP.val !=  nodeQ.val:
+            return False
+            
+        return same_tree_helper(nodeP.left, nodeQ.left) and same_tree_helper(nodeP.right, nodeQ.right)
+
+    return same_tree_helper(p, q)
+
+
+
+def same_tree_concise(p, q):
+    def same_tree_helper(nodeP, nodeQ):
+        if nodeP is None and nodeQ is None:
+            return True
+        elif nodeP and nodeQ:
+            return nodeP.val == nodeQ.val and \
+                same_tree_helper(nodeP.left, nodeQ.left) and \
+                same_tree_helper(nodeP.right, nodeQ.right)
+        
+        return False
+
+    return same_tree_helper(p, q)
