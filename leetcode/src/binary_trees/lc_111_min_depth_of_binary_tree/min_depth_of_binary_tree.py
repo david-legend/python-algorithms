1a47988d2ff63ca8ee982eb819619d6dc4a82ea5
David Cobbina <davidcobbina47@gmail.com>
2022-09-07 12:24:18 +0000

    added min_depth of a binary tree solution

---
+++ leetcode/src/binary_trees/lc_111_min_depth_of_binary_tree/min_depth_of_binary_tree.py
@@ -0,0 +1,53 @@
+from collections import deque
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left, self.right = left, right
+
+
+# Breadth First Search -> Level Order Traversal Solution
+
+def min_depth_bfs(root):
+    if root is None:
+        return 0
+    else:
+        queue = deque([root])
+        depth = 1
+
+        while queue:
+            level_size = len(queue)
+            for _ in range(level_size):
+                curr_node = queue.pop()
+
+                if curr_node.left is None and curr_node.right is None:
+                    return depth
+                
+                if curr_node.left:
+                    queue.append(curr_node.left)
+                
+                if curr_node.right:
+                    queue.append(curr_node.right)
+            
+            depth += 1
+            
+def min_depth_of_tree(root):
+
+    def min_depth_helper(node):
+
+        if node and node.left is None and node.right is None:
+            return 1
+        
+        left_result = float('inf')
+        if node.left:
+            left_result = min_depth_helper(node.left)
+
+        right_result = float('inf')
+        if node.right:
+            right_result = min_depth_helper(node.right)
+        
+        return min(left_result, right_result) + 1
+
+    if root:
+        return min_depth_helper(root)
+    
+    return 0
