3aa51b5ef9cbbdd71caffe4d1f6de3a64ed5fc0c
David Cobbina <davidcobbina47@gmail.com>
2022-09-11 15:11:19 +0000

    added find mode in bst

--- leetcode/src/binary_trees/lc_501_find_mode_in_bst/find_mode_in_bst.py
+++ leetcode/src/binary_trees/lc_501_find_mode_in_bst/find_mode_in_bst.py
@@ -1,5 +1,54 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from collections import defaultdict
+
+
+# inorder traversal
+# Time O(n) | Space O(n)
+def find_mode_in_bst(root):
+    def find_mode_in_bst_helper(node):
+        nonlocal count, max_count, prev
+        if node is None: return
+
+        find_mode_in_bst_helper(node.left)
+        if prev is not None:
+            if prev == node.val:
+                count += 1
+            else:
+                count = 1
+        
+        if count > max_count:
+            max_count = count
+            modes.clear()
+            modes.append(node.val)
+        elif count == max_count:
+            modes.append(node.val)
+        
+        prev = node.val
+        find_mode_in_bst_helper(node.right)
+
+    
+    count, max_count, prev = 1, 0, None
+    modes = []
+    find_mode_in_bst_helper(root)
+
+    return modes
+
+
+# Preorder traversal
+# Time O(n) | Space O(n)
+def find_mode_in_bst_preorder(root):
+    def find_mode_in_bst_helper(node):
+        nonlocal max_count
+        if node is None: return
+
+        values_to_count[node.val] += 1
+        count_to_values[values_to_count[node.val]].append(node.val)
+        max_count = max(max_count, values_to_count[node.val])
+
+        find_mode_in_bst_helper(node.left)
+        find_mode_in_bst_helper(node.right)
+
+    values_to_count, count_to_values = defaultdict(int), defaultdict(list)
+    max_count = 0
+    find_mode_in_bst_helper(root)
+    
+    return count_to_values[max_count]
