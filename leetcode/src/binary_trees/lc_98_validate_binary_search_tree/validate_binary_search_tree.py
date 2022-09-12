a57942bcd1b5aab91e1ccf664afd9639dd805ca7
David Cobbina <davidcobbina47@gmail.com>
2022-09-12 10:32:45 +0000

    added validate bst solution

---
+++ leetcode/src/binary_trees/lc_98_validate_binary_search_tree/validate_binary_search_tree.py
@@ -0,0 +1,11 @@
+def valid_bst(root):
+    def valid_bst_helper(node, lower_bound, upper_bound):
+        if node is None: return True
+
+        if not (lower_bound < node.val < upper_bound):
+            return False
+        
+        return (valid_bst_helper(node.left, lower_bound, node.val) and 
+                valid_bst_helper(node.right, node.val, upper_bound))
+
+    return valid_bst_helper(root, float('-inf'), float('inf'))
