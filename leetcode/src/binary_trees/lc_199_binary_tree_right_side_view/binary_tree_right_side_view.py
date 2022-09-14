0285f26d3538ba4390985fef70a4ea5bc40eed7d
David Cobbina <davidcobbina47@gmail.com>
2022-09-14 12:42:58 +0000

    added right side view of tree

--- leetcode/src/binary_trees/lc_199_binary_tree_right_side_view/binary_tree_right_side_view.py
+++ leetcode/src/binary_trees/lc_199_binary_tree_right_side_view/binary_tree_right_side_view.py
@@ -1,1 +1,21 @@
+from collections import deque
 
+def rightSideView(root):
+    if not root:
+        return []
+    
+    queue, result = deque([root]), []
+    while queue:
+        level_size = len(queue)
+        for i in range(level_size):
+            curr_node = queue.popleft()
+            
+            if i == level_size - 1:
+                result.append(curr_node.val)
+            
+            if curr_node.left:
+                queue.append(curr_node.left)
+            if curr_node.right:
+                queue.append(curr_node.right)
+    
+    return result
