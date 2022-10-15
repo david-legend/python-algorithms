597bfe4be00be8b67664a27f6854fcc2b895938e
David Cobbina <davidcobbina47@gmail.com>
2022-10-15 21:56:15 +0000

    added implementation of trie

---
+++ leetcode/src/trie/lc_208_implement_trie/implement_trie.py
@@ -0,0 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            
+            curr = curr.children[char]
+        curr.end_of_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        
+        return curr.end_of_word
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        
+        return True
